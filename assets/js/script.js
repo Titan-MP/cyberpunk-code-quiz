92df6ecca5a68816a8311b17c6faa1721c51f75c
Titan-MP <marcrhymaun@knights.ucf.edu>
2023-05-11 18:01:12 -0400

    added all questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@
 const clock = document.getElementById('clock');
 const main = document.querySelector('main');
 const start = document.getElementById('start');
-let time = -8;
+const alert = document.getElementById('alert');
+let time = 60;
+let qI = 0;
 
-start.onclick = () => {
+const runClock = () => {
 
     let clockId = setInterval(() => {
         time--;
@@ -16,3 +18,42 @@ start.onclick = () => {
     },1000);
 }
 
+const handleAns = answer => {
+    if (answer === questions[qI].C) {
+        alert.innerHTML = '<h1 style="color:green">Correct!!!</h1>';
+
+        setTimeout(() => {alert.innerHTML = ''}, 1000);
+    } else {
+        console.log('incorrect');
+        time -= 10;
+        alert.innerHTML = '<h1 style="color:red">Incorrect!!!</h1>';
+
+        setTimeout(() => {alert.innerHTML = ''}, 1000);
+    }
+    qI++;
+    if (qI < questions.length) {
+        renderQuestion();
+    } else {
+        console.log('game over');
+    }
+};
+
+const renderQuestion = () => {
+    let {Q, A, C} = questions[qI];
+
+    main.innerHTML = `<h1>${Q}</h1><div id="answers"></div>`;
+    let answers = document.getElementById('answers');
+
+    A.forEach(answer => {
+        answers.innerHTML += `<button onclick="handleAns('${answer}')">${answer}</button>`;
+    });
+};
+
+const runQuiz = () => {
+    runClock();
+    renderQuestion();
+}
+
+start.onclick = runQuiz;
+
+runQuiz();
