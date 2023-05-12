dcbb4426b2d9e50b6f82f548abe10b42d112339c
Titan-MP <marcrhymaun@knights.ucf.edu>
2023-05-11 22:15:54 -0400

    added right questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,27 @@
 const questions = [
     {
-        Q: 'Question 1?',
-        A: ['answer 1', 'answer 2', 'answer 3', 'answer 4'],
-        C: 'answer 3'
+        Q: 'Question1 - What is JavaScript?',
+        A: ['a dog', 'a country', 'a programming language', 'all of the above'],
+        C: 'a programming language'
     },
     {
-        Q: 'Question 2?',
-        A: ['answer 1', 'answer 2', 'answer 3', 'answer 4'],
-        C: 'answer 3'
+        Q: 'Question 2 - What are the different types of data that JavaScript cannot store?',
+        A: ['strings', 'numbers', 'people', 'objects'],
+        C: 'people'
     },
     {
-        Q: 'Question 3?',
-        A: ['answer 1', 'answer 2', 'answer 3', 'answer 4'],
-        C: 'answer 3'
+        Q: 'Question 3 - What are some of the basic JavaScript syntax?',
+        A: ['vaiables', 'all of them', 'functions', 'arrays'],
+        C: 'all of them'
     },
     {
-        Q: 'Question 4?',
-        A: ['answer 1', 'answer 2', 'answer 3', 'answer 4'],
-        C: 'answer 3'
+        Q: 'Question 4 - What is a common JavaScript error?',
+        A: ['Syntax', 'kissing', 'hugging', 'walking'],
+        C: 'Syntax'
     },
     {
-        Q: 'Question 5?',
-        A: ['answer 1', 'answer 2', 'answer 3', 'answer 4'],
-        C: 'answer 3'
+        Q: 'Question 5 - Where can I learn more about JavaScript? ',
+        A: ['the pool', 'the club', 'online', 'the beach'],
+        C: 'online'
     },
-]
+];
